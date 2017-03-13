13ebc8be63c2627a626dbea1d70fae1c8a888555
Oleksii Holub <Oleksii.Holub@shape.com>
2017-03-13 14:07:06 +0200

    Add XElement.StripNamespaces

---
+++ Extensions/Ext.Xml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using JetBrains.Annotations;
+
+namespace Tyrrrz.Extensions
+{
+    public static partial class Ext
+    {
+        /// <summary>
+        /// Strips namespaces from this element, its attributes and repeats the same for all its children
+        /// </summary>
+        [Pure, NotNull]
+        public static XElement StripNamespaces([NotNull] this XElement element)
+        {
+            // Original code credit: http://stackoverflow.com/a/1147012
+
+            if (element == null)
+                throw new ArgumentNullException(nameof(element));
+
+            var result = new XElement(element);
+            foreach (var e in result.DescendantsAndSelf())
+            {
+                e.Name = XNamespace.None.GetName(e.Name.LocalName);
+                var attributes = e.Attributes()
+                    .Where(a => !a.IsNamespaceDeclaration)
+                    .Where(a => a.Name.Namespace != XNamespace.Xml && a.Name.Namespace != XNamespace.Xmlns)
+                    .Select(a => new XAttribute(XNamespace.None.GetName(a.Name.LocalName), a.Value));
+                e.ReplaceAttributes(attributes);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExtensionsTests/XmlTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyrrrz.Extensions.Tests
+{
+    [TestClass]
+    public class XmlTests
+    {
+        [TestMethod]
+        public void StripNamespacesTest()
+        {
+            var ns = XNamespace.Get("http://schemas.domain.com/orders");
+            var xml =
+                new XElement(ns + "order",
+                    new XElement(ns + "customer", "Foo", new XAttribute(ns + "hello", "world")),
+                    new XElement("purchases",
+                        new XElement(ns + "purchase", "Unicycle", new XAttribute("price", "100.00")),
+                        new XElement("purchase", "Bicycle"),
+                        new XElement(ns + "purchase", "Tricycle",
+                            new XAttribute("price", "300.00"),
+                            new XAttribute(XNamespace.Xml.GetName("space"), "preserve")
+                        )
+                    )
+                );
+
+            var stripped = xml.StripNamespaces();
+            var xCustomer = stripped.Element("customer");
+            var xHello = xCustomer?.Attribute("hello");
+
+            Assert.IsNotNull(xCustomer);
+            Assert.IsNotNull(xHello);
+            Assert.AreEqual("world", xHello.Value);
+        }
+    }
+}
