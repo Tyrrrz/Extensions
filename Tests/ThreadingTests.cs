2d8ba7ff834ea50b8fb1460d21773d0dac0c5ed0
Oleksii Holub <Oleksii.Holub@shape.com>
2017-03-29 15:04:14 +0300

    Add ParallelSelectAsync

--- Tests/ThreadingTests.cs
+++ Tests/ThreadingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tyrrrz.Extensions.Tests
@@ -11,21 +12,24 @@ namespace Tyrrrz.Extensions.Tests
         {
             var a = new[] {1, 5, 9, -5, 11};
 
-            int aSum1 = 0;
-            int aSum2 = 0;
+            int aSum = 0;
 
             await a.ParallelForEachAsync(i =>
             {
-                aSum1 += i;
-            });
-            await a.ParallelForEachAsync(async i =>
-            {
-                await Task.Yield();
-                aSum2 += i;
+                aSum += i;
             });
 
-            Assert.AreEqual(21, aSum1);
-            Assert.AreEqual(21, aSum2);
+            Assert.AreEqual(21, aSum);
+        }
+
+        [TestMethod]
+        public async Task ParallelSelectAsyncTest()
+        {
+            var a = new[] { 1, 5, 9, -5, 11 };
+
+            var aStr = (await a.ParallelSelectAsync(i => i.ToString())).ToArray();
+
+            CollectionAssert.AreEquivalent(new[] {"1", "5", "9", "-5", "11"}, aStr);
         }
     }
 }
