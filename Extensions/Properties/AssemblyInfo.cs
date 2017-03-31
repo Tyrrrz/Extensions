fa9f65b2016d2f886a17b87a3fa76e7911133d7f
Tyrrrz <tyrrrrrr@gmail.com>
2017-03-31 16:25:03 +0300

    Update version

--- Extensions/Properties/AssemblyInfo.cs
+++ Extensions/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
