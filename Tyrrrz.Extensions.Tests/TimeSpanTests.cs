d5035789c8285396db0178691131f9287ce6e47b
Alexey Golub <Tyrrrrrr@gmail.com>
2017-12-24 14:11:25 +0200

    Add some TimeSpan extensions

---
+++ Tyrrrz.Extensions.Tests/TimeSpanTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyrrrz.Extensions.Tests
+{
+    [TestClass]
+    public class TimeSpanTests
+    {
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            var t1 = TimeSpan.FromMinutes(123);
+            var t2 = t1.Multiply(2);
+
+            Assert.AreEqual(t1.TotalSeconds * 2, t2.TotalSeconds, 10e-5);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            var t1 = TimeSpan.FromMinutes(123);
+            var t2 = t1.Divide(2);
+
+            Assert.AreEqual(t1.TotalSeconds / 2, t2.TotalSeconds, 10e-5);
+        }
+    }
+}
---
+++ Tyrrrz.Extensions/Ext.TimeSpan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tyrrrz.Extensions
+{
+    public static partial class Ext
+    {
+        /// <summary>
+        /// Multiplies the given timespan by a scalar value.
+        /// </summary>
+        public static TimeSpan Multiply(this TimeSpan timeSpan, double multiplier)
+        {
+            return TimeSpan.FromSeconds(timeSpan.TotalSeconds * multiplier);
+        }
+
+        /// <summary>
+        /// Divides the given timespan by a scalar value.
+        /// </summary>
+        public static TimeSpan Divide(this TimeSpan timeSpan, double divider)
+        {
+            return TimeSpan.FromSeconds(timeSpan.TotalSeconds / divider);
+        }
+    }
+}
