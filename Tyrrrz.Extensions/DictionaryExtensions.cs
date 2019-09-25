763c530617afded0dea4c9f149767b74e81e7d7f
Alexey Golub <Tyrrrrrr@gmail.com>
2019-09-25 19:22:04 +0300

    Hide IDictionary.GetValueOrDefault on netstd2.1+ to avoid ambiguity

--- Tyrrrz.Extensions/DictionaryExtensions.cs
+++ Tyrrrz.Extensions/DictionaryExtensions.cs
@@ -9,6 +9,7 @@ namespace Tyrrrz.Extensions
     /// </summary>
     public static class DictionaryExtensions
     {
+#if !NETSTANDARD2_1
         /// <summary>
         /// Returns a value that corresponds to the given key or default if the key doesn't exist.
         /// </summary>
@@ -18,5 +19,6 @@ namespace Tyrrrz.Extensions
             dictionary.GuardNotNull(nameof(dictionary));
             return dictionary.TryGetValue(key, out var result) ? result : default;
         }
+#endif
     }
 }
