931747783223204b85994d64be12c5035e250faa
Alexey Golub <Tyrrrrrr@gmail.com>
2019-11-05 18:14:18 +0200

    Add AddRange for ICollection<T>

---
+++ Tyrrrz.Extensions.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tyrrrz.Extensions.Tests
+{
+    [TestFixture]
+    public class CollectionExtensionsTests
+    {
+        [Test]
+        public void AddRange_Test()
+        {
+            // Arrange
+            var collection = new List<string>() as ICollection<string>;
+            var items = new[]
+            {
+                "hello",
+                "world"
+            };
+
+            // Act
+            collection.AddRange(items);
+
+            // Assert
+            Assert.That(collection, Is.EqualTo(items));
+        }
+    }
+}
---
+++ Tyrrrz.Extensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tyrrrz.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="ICollection{T}" />.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Adds multiple items to the collection.
+        /// </summary>
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                collection.Add(item);
+        }
+    }
+}
